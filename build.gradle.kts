6d38c4481155ff225f7743135ee46184163e0ccd
LaoLittle <y1941803875@outlook.com>
2021-12-22 16:42:17 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
-    val kotlinVersion = "1.5.30"
+    val kotlinVersion = "1.6.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.9.0-RC2"
+    id("net.mamoe.mirai-console") version "2.9.0-M1"
 }
 
 group = "org.laolittle.plugin.BotSelfCensor"
-version = "1.0"
+version = "1.0.1"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public")
--- src/main/kotlin/BotSelfCensor.kt
+++ src/main/kotlin/BotSelfCensor.kt
@@ -5,18 +5,21 @@ import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.event.events.MessagePreSendEvent
+import net.mamoe.mirai.event.subscribeMessages
+import net.mamoe.mirai.message.data.Image
 import net.mamoe.mirai.message.data.PlainText
-import net.mamoe.mirai.message.data.content
+import net.mamoe.mirai.message.data.toMessageChain
 import net.mamoe.mirai.utils.error
 import net.mamoe.mirai.utils.info
-import org.laolittle.plugin.api.BaiduAIPAPI.censor
 import org.laolittle.plugin.api.BaiduAIPAPI.getToken
+import org.laolittle.plugin.api.BaiduAIPAPI.imageCensor
+import org.laolittle.plugin.api.BaiduAIPAPI.textCensor
 
 object BotSelfCensor : KotlinPlugin(
     JvmPluginDescription(
         id = "org.laolittle.plugin.BotSelfCensor",
         name = "BotSelfCensor",
-        version = "1.0",
+        version = "1.0.1",
     ) {
         author("LaoLittle")
     }
@@ -28,29 +31,46 @@ object BotSelfCensor : KotlinPlugin(
         if (Config.verifyType == Config.ApiType.Sample && Data.cookie.isBlank()) logger.error { "未设置Cookie！" }
         this.launch {
             if (Data.access_token.isEmpty()) {
-                Data.access_token = getToken()
+                run { Data.access_token = getToken() }
             }
         }
         logger.info { "Plugin loaded" }
         GlobalEventChannel.subscribeAlways<MessagePreSendEvent> {
-            val result = message.content.censor().toString()
-            runCatching {
-                message = when (result) {
-                    "2" -> PlainText(Config.illegalMessage.random())
-                    else -> message
+            for (node in message.toMessageChain()) {
+                var conclusionType = "1"
+                runCatching {
+                    val result = when (node) {
+                        is PlainText -> {
+                            node.content.textCensor().toString()
+                        }
+                        is Image -> {
+                            node.imageCensor().toString()
+                        }
+                        else -> {
+                            "1"
+                        }
+                    }
+                    message = when (result) {
+                        "2" -> {
+                            conclusionType = result
+                            PlainText(Config.illegalMessage.random())
+                        }
+                        else -> message
+                    }
+                }.onFailure {
+                    logger.error { "请求失败！" }
                 }
-            }.onFailure {
-                logger.error { "请求失败！" }
+                if (conclusionType == "2") break
             }
         }
 
-        /*  GlobalEventChannel.subscribeMessages {
-              startsWith("发"){
-                  if (it.isEmpty()) return@startsWith
-                  subject.sendMessage(it)
-              }
-          }
+        GlobalEventChannel.subscribeMessages {
+            startsWith("发") {
+                if (it.isEmpty()) return@startsWith
+                subject.sendMessage(message)
+            }
+        }
 
-         */
+
     }
 }
